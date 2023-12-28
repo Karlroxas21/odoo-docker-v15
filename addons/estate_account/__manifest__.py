48004b1c9d3be8486ce45749e75b480da8f1c1d8
Karl Marx Roxas <karlmarxroxas1@gmail.com>
2023-12-28 09:17:43 +0800

    finish ch14

---
+++ addons/estate_account/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Estate Account',
+    'version': '1.0',
+    'sequence': 2,
+    'category': 'Real Estate',
+    'summary': 'Estate Account',
+    'description': "",
+    'website': 'https://google.com',
+    'depends': [
+            'estate',
+            'account'
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
---
+++ addons/estate_account/model/estate_property.py
@@ -0,0 +1,28 @@
+from odoo import models
+
+class EstateProperty(models.Model):
+        _inherit = 'real.estate.karl'
+
+        def sold_action(self):
+                partner_id = self.offer_ids.partner_id
+
+                # 6% of the selling price
+                selling_price_percentage = 0.06 * self.selling_price
+
+                move_vals = {
+                        'partner_id': partner_id,
+                        'move_type': 'out_invoice',
+                        'invoice_line_ids':[(0,0,{
+                                'name': 'Selling Price Percentage',
+                                'quantity': '1',
+                                'price_unit' : selling_price_percentage
+                        }), (0, 0, {
+                                'name': 'Administrative Fees',
+                                'quantity': 1,
+                                'price_unit': 100.00
+                        })]
+                }                     
+
+                self.env['account.move'].create(move_vals)
+                return super(EstateProperty, self).sold_action()
+
